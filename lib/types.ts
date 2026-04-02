f68828bd05e078ef5f35cb17c57a275fb36e58ad
David Oost <david@newblack.io>
2026-04-02 14:24:29 +0200

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -24,3 +24,78 @@ export type MigrationProduct = {
   Name: string;
   TaxCode: string;
 };
+
+export type ImportProducts = {
+  AllowEmptyMedia: boolean; // always true (delete images if not included in the request)
+  DownloadMediaInBackground: boolean; // always true (download images asynchronously)
+  Products: Product; // TODO
+  SystemID: string; // "eva-simple-pim"
+  Type: number; // 0
+  WaitForProductComposition: boolean; // always false
+};
+
+export type Product = {
+  Barcodes?: string[]; // barcodes for this product
+  Content: ProductContent[];
+  ID: string; // product identifier (we should generate this)
+  IsDeleted?: boolean; // set to true to delete product
+  LedgerClassID?: string;
+  Name: string;
+  TaxCode: string;
+  Type: ProductTypes;
+  VariationValues?: ProductVariationValue[];
+  Variations: ProductVariation[];
+  GiftCardConfigurationID?: string;
+  GiftCardIsDigital?: boolean;
+};
+
+export type ProductTypes = {
+  AllowCustomPricing: boolean;
+  BundleProduct: boolean;
+  GiftCard: boolean;
+  NotReturnable: boolean;
+  Service: boolean;
+  Stock: boolean;
+  SubscriptionProduct: boolean;
+  VirtualProduct: boolean;
+};
+
+export type ProductContent = {
+  LanguageID?: string; // 2 letter iso code
+  LongDescription: string;
+  Media: ProductMedia[];
+  Name: string;
+  PublicationStatuses: PublicationStatus[];
+  ShortDescription?: string;
+  XDimension?: Dimension;
+  YDimension?: Dimension;
+};
+
+export type ProductMedia = {
+  ID: string;
+  IsPrimaryImage: boolean;
+  MediaUrl: string;
+  Sequence: number;
+};
+
+export type Dimension = {
+  property: string;
+  value: string;
+};
+
+export type PublicationStatus = "public";
+
+export type ProductVariationValue = {
+  LanguageID?: string; // 2 letter iso code
+  Value: string;
+};
+
+export type ProductVariation = {
+  LogicalLevel: string;
+  Property: string;
+  Products: string; // todo
+};
+
+// Onboarding flow:
+// - Do you have a default language for content?
+// - Do you have a default Brand name?
